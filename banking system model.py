e969292b1228f6c14341eaf222757433e3706cae
Kimiagar <vahidito@gmail.com>
2021-03-22 19:12:19 +0430

    probability of default

--- banking system model.py
+++ banking system model.py
@@ -5,29 +5,36 @@ import matplotlib as plt
 #import sci
 
 
+##################################################################################
 
-### global
+### global variables
 
-# ret_sec_bank
+ret_sec_bank = 0.3
 # ret_sec_shbank
-# rfree
+rfree = 0.1
 
+########################################################################
 # defining the agents: banks, shadow banks, savers, loans
 
 
 class Bank:
-    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, equity, CAR, provision_per):
+    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, equity, CAR, provision_per, phi, zeta):
         # balance sheet
         self.bank_cash = bank_cash
         self.lend_to_banks = lend_to_banks
         self.lend_to_loans = lend_to_loans
         self.bank_sec = bank_sec
-        self.reserves = reserves
+        self.deposits = deposits
         self.borrow_from_banks = borrow_from_banks
-        self.equity = bank_cash + lend_to_banks + lend_to_loans + bank_sec - reserves - borrow_from_banks
+        self.equity = equity
         self.CAR = CAR
         self.provision_per = provision_per
         ##### income and expense of bank
+        ini = 0.1
+        self.mu = (ret_sec_bank * bank_sec) + (rfree * lend_to_banks ) - ((rfree * borrow_from_banks)/(1 - zeta * ini))
+        sigma = phi * ()
+        profit = np.random.normal(self.mu, sigma, 1)
+
 
 
 
@@ -38,9 +45,16 @@ class Shadow_Bank:
         self.security = security
         ##### income and expense of shadow bank
 
+###############################################################
+#### defining probability of default
+
+
+
+
+###############################################################
 # introduction of Iranian Banks
 
-bank_melli = Bank(0,0,0,0,0,0,0,0,0)
+bank_melli = Bank(0,0,0,0,0,0,0,0,0,0,0)
 bank_seppah = Bank(0,0,0,0,0,0,0,0,0)
 bank_tosesaderat = Bank(0,0,0,0,0,0,0,0,0)
 bank_maskan = Bank(0,0,0,0,0,0,0,0,0)
@@ -67,5 +81,15 @@ bank_sarmaye = Bank(0,0,0,0,0,0,0,0,0)
 bank_pasargad = Bank(0,0,0,0,0,0,0,0,0)
 bank_melal = Bank(0,0,0,0,0,0,0,0,0)
 
-
+#####################################################
 # optimixation phase
+
+
+
+########################################################
+# simulations
+
+
+
+
+#######################################################
