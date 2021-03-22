6da5e74159246d4c81084b0e86cfd3d4ba53b6f6
Kimiagar <vahidito@gmail.com>
2021-03-22 11:22:55 +0430

    introduction of iranian banks

--- banking system model.py
+++ banking system model.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import numpy as np
+import mesa as mesa
+import pandas as pd
+import matplotlib as plt
+#import sci
+# defining the agents: banks, shadow banks, savers, loans
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Bank:
+    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, CAR, provision_per):
+        # balance sheet
+        self.bank_cash = bank_cash
+        self.lend_to_banks = lend_to_banks
+        self.lend_to_loans = lend_to_loans
+        self.bank_sec = bank_sec
+        self.reserves = reserves
+        self.borrow_from_banks = borrow_from_banks
+        self.equity = bank_cash + lend_to_banks + lend_to_loans + bank_sec - reserves - borrow_from_banks
+        self.CAR = CAR
+        self.provision_per = provision_per
+        ##### income and expense
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+class Shadow_Bank:
+    def __init__(self, participation, shadow_bank_cash):
+        self.participation = participation
+        self.shadow_bank_cash = shadow_bank_cash
+        self.security = participation - shadow_bank_cash
+
+# introduction of Iranian Banks
+
+bank_melli = Bank(0,0,0,0,0,0,0,0)
+bank_seppah = Bank(0,0,0,0,0,0,0,0)
+bank_tosesaderat = Bank(0,0,0,0,0,0,0,0)
+bank_maskan = Bank(0,0,0,0,0,0,0,0)
+bank_sanatmadan = Bank(0,0,0,0,0,0,0,0)
+bank_keshavarzi = Bank(0,0,0,0,0,0,0,0)
+bank_tosetavon = Bank(0,0,0,0,0,0,0,0)
+bank_post = Bank(0,0,0,0,0,0,0,0)
+bank_eghtesadnovin = Bank(0,0,0,0,0,0,0,0)
+bank_parsian = Bank(0,0,0,0,0,0,0,0)
+bank_karafarin = Bank(0,0,0,0,0,0,0,0)
+bank_saman = Bank(0,0,0,0,0,0,0,0)
+bank_sina = Bank(0,0,0,0,0,0,0,0)
+bank_khavarmiane = Bank(0,0,0,0,0,0,0,0)
+bank_shahr = Bank(0,0,0,0,0,0,0,0)
+bank_dey = Bank(0,0,0,0,0,0,0,0)
+bank_saderat = Bank(0,0,0,0,0,0,0,0)
+bank_tejarat = Bank(0,0,0,0,0,0,0,0)
+bank_mellat = Bank(0,0,0,0,0,0,0,0)
+bank_refah = Bank(0,0,0,0,0,0,0,0)
+bank_ayandeh = Bank(0,0,0,0,0,0,0,0)
+bank_gardeshgary = Bank(0,0,0,0,0,0,0,0)
+bank_iranzamin = Bank(0,0,0,0,0,0,0,0)
+bank_sarmaye = Bank(0,0,0,0,0,0,0,0)
+bank_pasargad = Bank(0,0,0,0,0,0,0,0)
+bank_melal = Bank(0,0,0,0,0,0,0,0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# optimixation phase
---
+++ simplex.py
@@ -0,0 +1,24 @@
+import numpy as np
+import scipy as spy
+from scipy.optimize import linprog
+
+c = np.array([-29.0, -45.0, 0.0, 0.0])
+A_ub = np.array([[1.0, -1.0, -3.0, 0.0], [-2.0, 3.0, 7.0, -3.0]])
+b_ub = np.array([5.0, -10.0])
+A_eq = np.array([[2.0, 8.0, 1.0, 0.0], [4.0, 4.0, 0.0, 1.0]])
+b_eq = np.array([60.0, 60.0])
+x0_bounds = (0, None)
+x1_bounds = (0, 5.0)
+x2_bounds = (-np.inf, 0.5)  # +/- np.inf can be used instead of None
+x3_bounds = (-3.0, None)
+bounds = [x0_bounds, x1_bounds, x2_bounds, x3_bounds]
+result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+print(result.x)
+xxxx = result.x
+print(type(xxxx))
+aaa = list(xxxx)
+print(type(aaa))
+aaa.append(1)
+print(aaa)
+a = aaa[1]
+print(type(a))
