600bc7bd9eb4350ded6f49f9f99eb6cd927b520e
Kimiagar <vahidito@gmail.com>
2021-03-22 18:21:59 +0430

    introduction of iranian banks

--- banking system model.py
+++ banking system model.py
@@ -3,10 +3,20 @@ import mesa as mesa
 import pandas as pd
 import matplotlib as plt
 #import sci
+
+
+
+### global
+
+# ret_sec_bank
+# ret_sec_shbank
+# rfree
+
 # defining the agents: banks, shadow banks, savers, loans
 
+
 class Bank:
-    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, CAR, provision_per):
+    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, equity, CAR, provision_per):
         # balance sheet
         self.bank_cash = bank_cash
         self.lend_to_banks = lend_to_banks
@@ -17,7 +27,7 @@ class Bank:
         self.equity = bank_cash + lend_to_banks + lend_to_loans + bank_sec - reserves - borrow_from_banks
         self.CAR = CAR
         self.provision_per = provision_per
-        ##### income and expense
+        ##### income and expense of bank
 
 
 
@@ -26,35 +36,36 @@ class Shadow_Bank:
         self.participation = participation
         self.shadow_bank_cash = shadow_bank_cash
         self.security = participation - shadow_bank_cash
+        ##### income and expense of shadow bank
 
 # introduction of Iranian Banks
 
-bank_melli = Bank(0,0,0,0,0,0,0,0)
-bank_seppah = Bank(0,0,0,0,0,0,0,0)
-bank_tosesaderat = Bank(0,0,0,0,0,0,0,0)
-bank_maskan = Bank(0,0,0,0,0,0,0,0)
-bank_sanatmadan = Bank(0,0,0,0,0,0,0,0)
-bank_keshavarzi = Bank(0,0,0,0,0,0,0,0)
-bank_tosetavon = Bank(0,0,0,0,0,0,0,0)
-bank_post = Bank(0,0,0,0,0,0,0,0)
-bank_eghtesadnovin = Bank(0,0,0,0,0,0,0,0)
-bank_parsian = Bank(0,0,0,0,0,0,0,0)
-bank_karafarin = Bank(0,0,0,0,0,0,0,0)
-bank_saman = Bank(0,0,0,0,0,0,0,0)
-bank_sina = Bank(0,0,0,0,0,0,0,0)
-bank_khavarmiane = Bank(0,0,0,0,0,0,0,0)
-bank_shahr = Bank(0,0,0,0,0,0,0,0)
-bank_dey = Bank(0,0,0,0,0,0,0,0)
-bank_saderat = Bank(0,0,0,0,0,0,0,0)
-bank_tejarat = Bank(0,0,0,0,0,0,0,0)
-bank_mellat = Bank(0,0,0,0,0,0,0,0)
-bank_refah = Bank(0,0,0,0,0,0,0,0)
-bank_ayandeh = Bank(0,0,0,0,0,0,0,0)
-bank_gardeshgary = Bank(0,0,0,0,0,0,0,0)
-bank_iranzamin = Bank(0,0,0,0,0,0,0,0)
-bank_sarmaye = Bank(0,0,0,0,0,0,0,0)
-bank_pasargad = Bank(0,0,0,0,0,0,0,0)
-bank_melal = Bank(0,0,0,0,0,0,0,0)
+bank_melli = Bank(0,0,0,0,0,0,0,0,0)
+bank_seppah = Bank(0,0,0,0,0,0,0,0,0)
+bank_tosesaderat = Bank(0,0,0,0,0,0,0,0,0)
+bank_maskan = Bank(0,0,0,0,0,0,0,0,0)
+bank_sanatmadan = Bank(0,0,0,0,0,0,0,0,0)
+bank_keshavarzi = Bank(0,0,0,0,0,0,0,0,0)
+bank_tosetavon = Bank(0,0,0,0,0,0,0,0,0)
+bank_post = Bank(0,0,0,0,0,0,0,0,0)
+bank_eghtesadnovin = Bank(0,0,0,0,0,0,0,0,0)
+bank_parsian = Bank(0,0,0,0,0,0,0,0,0)
+bank_karafarin = Bank(0,0,0,0,0,0,0,0,0)
+bank_saman = Bank(0,0,0,0,0,0,0,0,0)
+bank_sina = Bank(0,0,0,0,0,0,0,0,0)
+bank_khavarmiane = Bank(0,0,0,0,0,0,0,0,0)
+bank_shahr = Bank(0,0,0,0,0,0,0,0,0)
+bank_dey = Bank(0,0,0,0,0,0,0,0,0)
+bank_saderat = Bank(0,0,0,0,0,0,0,0,0)
+bank_tejarat = Bank(0,0,0,0,0,0,0,0,0)
+bank_mellat = Bank(0,0,0,0,0,0,0,0,0)
+bank_refah = Bank(0,0,0,0,0,0,0,0,0)
+bank_ayandeh = Bank(0,0,0,0,0,0,0,0,0)
+bank_gardeshgary = Bank(0,0,0,0,0,0,0,0,0)
+bank_iranzamin = Bank(0,0,0,0,0,0,0,0,0)
+bank_sarmaye = Bank(0,0,0,0,0,0,0,0,0)
+bank_pasargad = Bank(0,0,0,0,0,0,0,0,0)
+bank_melal = Bank(0,0,0,0,0,0,0,0,0)
 
 
 # optimixation phase
