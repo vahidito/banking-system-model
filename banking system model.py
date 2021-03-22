2df03a5a1f1a2dd63d21e26789dab44c588bc96d
Kimiagar <vahidito@gmail.com>
2021-03-22 19:39:18 +0430

    probability of default

--- banking system model.py
+++ banking system model.py
@@ -2,7 +2,8 @@ import numpy as np
 import mesa as mesa
 import pandas as pd
 import matplotlib as plt
-#import sci
+import scipy as spy
+from scipy.stats import norm
 
 
 ##################################################################################
@@ -18,7 +19,7 @@ rfree = 0.1
 
 
 class Bank:
-    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, reserves, borrow_from_banks, equity, CAR, provision_per, phi, zeta):
+    def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, deposits, borrow_from_banks, equity, CAR, provision_per, phi, zeta):
         # balance sheet
         self.bank_cash = bank_cash
         self.lend_to_banks = lend_to_banks
@@ -31,10 +32,10 @@ class Bank:
         self.provision_per = provision_per
         ##### income and expense of bank
         ini = 0.1
-        self.mu = (ret_sec_bank * bank_sec) + (rfree * lend_to_banks ) - ((rfree * borrow_from_banks)/(1 - zeta * ini))
-        sigma = phi * ()
+        self.mu = (ret_sec_bank * bank_sec) + (rfree * lend_to_banks) - ((rfree * borrow_from_banks)/(1 - zeta * ini))
+        sigma = phi * (deposits + equity)
         profit = np.random.normal(self.mu, sigma, 1)
-
+        pd = norm.ppf((equity - profit)/(sigma))
 
 
 
@@ -54,32 +55,32 @@ class Shadow_Bank:
 ###############################################################
 # introduction of Iranian Banks
 
-bank_melli = Bank(0,0,0,0,0,0,0,0,0,0,0)
-bank_seppah = Bank(0,0,0,0,0,0,0,0,0)
-bank_tosesaderat = Bank(0,0,0,0,0,0,0,0,0)
-bank_maskan = Bank(0,0,0,0,0,0,0,0,0)
-bank_sanatmadan = Bank(0,0,0,0,0,0,0,0,0)
-bank_keshavarzi = Bank(0,0,0,0,0,0,0,0,0)
-bank_tosetavon = Bank(0,0,0,0,0,0,0,0,0)
-bank_post = Bank(0,0,0,0,0,0,0,0,0)
-bank_eghtesadnovin = Bank(0,0,0,0,0,0,0,0,0)
-bank_parsian = Bank(0,0,0,0,0,0,0,0,0)
-bank_karafarin = Bank(0,0,0,0,0,0,0,0,0)
-bank_saman = Bank(0,0,0,0,0,0,0,0,0)
-bank_sina = Bank(0,0,0,0,0,0,0,0,0)
-bank_khavarmiane = Bank(0,0,0,0,0,0,0,0,0)
-bank_shahr = Bank(0,0,0,0,0,0,0,0,0)
-bank_dey = Bank(0,0,0,0,0,0,0,0,0)
-bank_saderat = Bank(0,0,0,0,0,0,0,0,0)
-bank_tejarat = Bank(0,0,0,0,0,0,0,0,0)
-bank_mellat = Bank(0,0,0,0,0,0,0,0,0)
-bank_refah = Bank(0,0,0,0,0,0,0,0,0)
-bank_ayandeh = Bank(0,0,0,0,0,0,0,0,0)
-bank_gardeshgary = Bank(0,0,0,0,0,0,0,0,0)
-bank_iranzamin = Bank(0,0,0,0,0,0,0,0,0)
-bank_sarmaye = Bank(0,0,0,0,0,0,0,0,0)
-bank_pasargad = Bank(0,0,0,0,0,0,0,0,0)
-bank_melal = Bank(0,0,0,0,0,0,0,0,0)
+bank_melli = Bank(1,1,1,1,1,1,1,1,1,1,1)
+bank_seppah = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_tosesaderat = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_maskan = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_sanatmadan = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_keshavarzi = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_tosetavon = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_post = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_eghtesadnovin = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_parsian = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_karafarin = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_saman = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_sina = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_khavarmiane = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_shahr = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_dey = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_saderat = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_tejarat = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_mellat = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_refah = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_ayandeh = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_gardeshgary = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_iranzamin = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_sarmaye = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_pasargad = Bank(0,0,0,0,0,0,0,0,0,0,0)
+bank_melal = Bank(0,0,0,0,0,0,0,0,0,0,0)
 
 #####################################################
 # optimixation phase
@@ -93,3 +94,5 @@ bank_melal = Bank(0,0,0,0,0,0,0,0,0)
 
 
 #######################################################
+
+print(bank_melli.mu)
