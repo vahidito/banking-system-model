d350262a2c5a5d89d59a97a6c4b84cdec7ee0eb9
Kimiagar <vahidito@gmail.com>
2021-03-23 19:33:09 +0430

    create shadow banks

--- banking system model.py
+++ banking system model.py
@@ -90,7 +90,23 @@ shadow1 = Shadow_Bank(np.random.normal(100), np.random.normal(20))
 
 
 #####################################################
+# 1-BL 2-S 3-BB 4-L 5-C 6-D 7-E
 # optimixation phase
+c = np.array([rfree, ret_sec_bank,(rfree * self.borrow_from_banks) / (1 - zeta * pd)) , 0.0])
+A_ub = np.array([[1.0, -1.0, -3.0, 0.0], [-2.0, 3.0, 7.0, -3.0]])
+b_ub = np.array([5.0, -10.0])
+A_eq = np.array([[2.0, 8.0, 1.0, 0.0], [4.0, 4.0, 0.0, 1.0]])
+b_eq = np.array([60.0, 60.0])
+x0_bounds = (0, None)
+x1_bounds = (0, 5.0)
+x2_bounds = (-np.inf, 0.5)  # +/- np.inf can be used instead of None
+x3_bounds = (-3.0, None)
+bounds = [x0_bounds, x1_bounds, x2_bounds, x3_bounds]
+result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+print(result.x)
+
+
+
 
 
 ########################################################
