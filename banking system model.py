3d830a92ffd61b79415b83f6e859611a50bc0ccb
Kimiagar <vahidito@gmail.com>
2021-03-25 14:42:31 +0430

    optimization details

--- banking system model.py
+++ banking system model.py
@@ -10,7 +10,7 @@ from scipy.optimize import linprog
 
 ### global variables
 
-ret_sec_bank = 0.3
+ret_sec_bank = 0.1
 ret_sec_shbank = 0.35
 rfree = 0.1
 
@@ -21,7 +21,7 @@ rfree = 0.1
 
 class Bank:
     def __init__(self, bank_cash, lend_to_banks, lend_to_loans, bank_sec, deposits, borrow_from_banks, equity, CAR,
-                 provision_per, phi, zeta):
+                 provision_per, phi, zeta , landa_min, xs, xbl, xl):
         # balance sheet
         self.bank_cash = bank_cash
         self.lend_to_banks = lend_to_banks
@@ -33,6 +33,11 @@ class Bank:
         self.CAR = CAR
         self.provision_per = provision_per
         self.zeta = zeta
+        self.total_assets = bank_cash + lend_to_banks + lend_to_loans + bank_sec
+        self.xs = xs
+        self.xbl = xbl
+        self.xl = xl
+        self.landa_min = landa_min
 
         ##### income and expense of bank
         pd = 0.1
@@ -41,6 +46,7 @@ class Bank:
         self.sigma = phi * (deposits + equity)
         self.profit = float(np.random.normal(self.net_income, self.sigma, 1))
         self.pd = norm.cdf((-self.net_income - self.equity) / (self.sigma))
+        self.pd1 = norm.ppf((-self.net_income - self.equity) / (self.sigma))
 
 
 class Shadow_Bank:
@@ -54,7 +60,7 @@ class Shadow_Bank:
 ###############################################################
 # introduction of Iranian Banks
 
-bank_melli = Bank(500, 1000, 1000, 200, 900, 1500, 00, 0.1, 0.1, 0.1, 0.1)
+bank_melli = Bank(500, 1000, 1000, 200, 700, 1500, 500, 0.1, 0.1, 0.1, 0.1 , 0.07 , 0.05, 0.05, 0.05)
 # bank_seppah = Bank(0,0,0,0,0,0,0,0,0,0,0)
 # bank_tosesaderat = Bank(0,0,0,0,0,0,0,0,0,0,0)
 # bank_maskan = Bank(0,0,0,0,0,0,0,0,0,0,0)
@@ -94,16 +100,16 @@ shadow1 = Shadow_Bank(np.random.normal(100), np.random.normal(20))
 ### objective function
 
 c = np.array(
-    [-rfree, -ret_sec_bank, ((-rfree * bank_melli.borrow_from_banks) / (1 - bank_melli.zeta * bank_melli.pd)), 0, 0, 0, 0])
+    [-rfree, -ret_sec_bank, ((rfree * bank_melli.borrow_from_banks) / (1 - bank_melli.zeta * bank_melli.pd)), 0, 0, 0, 0])
 
-A_ub = np.array([[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0 ,0 ,0]])
+A_ub = np.array([[(-1 + bank_melli.landa_min * bank_melli.xbl), (-1 + bank_melli.landa_min * bank_melli.xs), 1, (-1 + bank_melli.landa_min * bank_melli.xl), -1, 1, 0], [0, 0, 0, 0, 0 ,0 ,0]])
 b_ub = np.array([0, 0])
 
 #### the first bound is structural balance sheet equation
 #### the second one is based on this assumtion that  half of the bank’s assets is invested in loans
 
-A_eq = np.array([[1, 1, -1, 1, 1, -1, -1], [0, 0, -1, 2, 0, -1, -1]])
-b_eq = np.array([0, 0])
+A_eq = np.array([[1, 1, -1, 1, 1, -1, -1], [0, 0, -1, 2, 0, -1, -1], [1, 1, 0, 1, 1, 0, 0] ])
+b_eq = np.array([0, 0, bank_melli.total_assets])
 
 x0_bounds = (0, None)
 x1_bounds = (0, None)
@@ -116,6 +122,10 @@ x6_bounds = (0, None)
 bounds = [x0_bounds, x1_bounds, x2_bounds, x3_bounds, x4_bounds, x5_bounds, x6_bounds]
 result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
 print(result.x)
+aaa = [result.x[2] , result.x[5] , result.x[6]]
+bbb = [result.x[0] , result.x[1] , result.x[3] , result.x[4]]
+print(sum(aaa))
+print(sum(bbb))
 print(result.x[1])
 print(sum(result.x))
 
@@ -127,5 +137,4 @@ print(sum(result.x))
 print(bank_melli.net_income)
 print(bank_melli.profit)
 print(bank_melli.pd)
-print(shadow1.participation)
-print(shadow1.shadow_bank_cash)
+print(bank_melli.pd1)
