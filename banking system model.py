8563312d806b702a19b2747fe335b5e2b7009ee8
Kimiagar <vahidito@gmail.com>
2021-04-08 16:51:46 +0430

    optimize shadow banks

--- banking system model.py
+++ banking system model.py
@@ -1,6 +1,4 @@
 import numpy as np
-import mesa as mesa
-import pandas as pd
 import matplotlib.pyplot as plt
 import scipy as spy
 from scipy.stats import norm
@@ -9,7 +7,7 @@ from scipy.optimize import linprog
 ##################################################################################
 
 ### global variables
-n_sim = 30
+n_sim = 15
 ret_sec_bank = 0.18
 ret_sec_bank_sigma = 0.01
 ret_sec_shbank = 0.18
@@ -23,6 +21,9 @@ p_market_max = 110
 p_market_min = 90
 p_market_vector = [f'{p_market}']
 ret_sec_bank_vector = [f'{ret_sec_bank}']
+
+every = 0
+every_thing_vector = [f'{every}']
 
 
 ########################################################################
@@ -56,12 +57,9 @@ class Bank:
         self.demand_of_stock_b = 0
 
         ##### income and expense of bank
-        pd = 0.1
-        self.net_income = (ret_sec_bank * self.bank_sec) + (rfree * self.lend_to_banks) - (
-                (rfree * self.borrow_from_banks) / (1 - self.zeta * pd))
         self.phi = phi
         self.sigma = phi * (self.deposits + self.equity)
-        self.profit = float(np.random.normal(self.net_income, self.sigma, 1))
+        # self.profit = float(np.random.normal(self.net_income, self.sigma, 1))
         # self.pd = float(norm.ppf((-self.net_income - self.equity) / (self.sigma)))
         self.pd = np.random.beta(1, 20)
 
@@ -75,6 +73,7 @@ class Shadow_Bank:
         self.s_alpha = s_alpha
         self.s_provision = s_provision
         self.int_value = np.random.normal(intrinsic_value)
+        self.redemption = 0
         self.stock = security / p_market
 
         ##### income and expense of shadow bank
@@ -137,46 +136,50 @@ shadow15 = Shadow_Bank(np.random.normal(100), np.random.normal(20), np.random.no
 ### objective function of banks
 
 def optimize_bank(mmm):
-    c = np.array([-rfree, -mmm.ret_on_sec, ((rfree * mmm.borrow_from_banks) / (1 - mmm.zeta * mmm.pd)), 0, 0])
-    A_ub = np.array([[(-1 + mmm.car * mmm.xbl), (-1 + mmm.car * mmm.xs), 1, (-1 + mmm.car * mmm.xl), -1],
-                     [-1, 0, (mmm.alpha_min + mmm.provision_per), 0, -1], [0, 0, 0, 0, -1]])
-    b_ub = np.array([-mmm.deposits, -(mmm.alpha_min + mmm.provision_per) * mmm.deposits,
-                     -(mmm.alpha_min + mmm.provision_per) * mmm.deposits])
-    A_eq = np.array([[1, 1, -1, 1, 1], [0, 0, -1, 2, 0]])
-    b_eq = np.array([mmm.deposits + mmm.equity, mmm.deposits + mmm.equity])
-    x0_bounds = (0, None)
-    x1_bounds = (0, None)
-    x2_bounds = (0, None)
-    x3_bounds = (0, None)
-    x4_bounds = (0, None)
-    bounds = [x0_bounds, x1_bounds, x2_bounds, x3_bounds, x4_bounds]
-    result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
-    mmm.bank_cash = result.x[4]
-    mmm.lend_to_banks = result.x[0]
-    mmm.lend_to_loans = result.x[3]
-    bank_sec_old = mmm.bank_sec
-    mmm.bank_sec = result.x[1]
-    mmm.borrow_from_banks = result.x[2]
-    mmm.ret_on_sec = float(np.random.normal(ret_sec_bank, ret_sec_bank_sigma))
-    mmm.net_income = (mmm.ret_on_sec * mmm.bank_sec) + (rfree * mmm.lend_to_banks) - (
-            (rfree * mmm.borrow_from_banks) / (1 - mmm.zeta * mmm.pd))
-    mmm.sigma = abs(mmm.phi * (mmm.deposits + mmm.equity))
-    mmm.profit = float(np.random.normal(mmm.net_income, mmm.sigma, 1))
+    if mmm.bankrupt == False:
+        c = np.array([-rfree, -mmm.ret_on_sec, ((rfree * mmm.borrow_from_banks) / (1 - mmm.zeta * mmm.pd)), 0, 0])
+        A_ub = np.array([[(-1 + mmm.car * mmm.xbl), (-1 + mmm.car * mmm.xs), 1, (-1 + mmm.car * mmm.xl), -1],
+                         [-1, 0, (mmm.alpha_min + mmm.provision_per), 0, -1], [0, 0, 0, 0, -1]])
+        b_ub = np.array([-mmm.deposits, -(mmm.alpha_min + mmm.provision_per) * mmm.deposits,
+                         -(mmm.alpha_min + mmm.provision_per) * mmm.deposits])
+        A_eq = np.array([[1, 1, -1, 1, 1], [0, 0, -1, 2, 0]])
+        b_eq = np.array([mmm.deposits + mmm.equity, mmm.deposits + mmm.equity])
+        x0_bounds = (0, None)
+        x1_bounds = (0, None)
+        x2_bounds = (0, None)
+        x3_bounds = (0, None)
+        x4_bounds = (0, None)
+        bounds = [x0_bounds, x1_bounds, x2_bounds, x3_bounds, x4_bounds]
+        result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds)
+        mmm.bank_cash = result.x[4]
+        mmm.lend_to_banks = result.x[0]
+        mmm.lend_to_loans = result.x[3]
+        bank_sec_old = mmm.bank_sec
+        mmm.bank_sec = result.x[1]
+        mmm.borrow_from_banks = result.x[2]
+        mmm.ret_on_sec = float(np.random.normal(ret_sec_bank, ret_sec_bank_sigma))
+        mmm.net_income = (mmm.ret_on_sec * mmm.bank_sec) + (rfree * mmm.lend_to_banks) - (
+                (rfree * mmm.borrow_from_banks) / (1 - mmm.zeta * mmm.pd))
+        mmm.sigma = abs(mmm.phi * (mmm.deposits + mmm.equity))
+        mmm.profit = float(np.random.normal(mmm.net_income, mmm.sigma, 1))
 
+        mmm.sec_sale = bank_sec_old - mmm.bank_sec
+        if mmm.sec_sale > 0:
+            mmm.demand_of_stock_b = 0
+            mmm.supply_of_stock_b = mmm.security_sale
+        elif mmm.security_sale < 0:
+            mmm.demand_of_stock_b = mmm.security_sale
+            mmm.supply_of_stock_b = 0
+        else:
+            mmm.demand_of_stock_b = 0
+            mmm.supply_of_stock_b = 0
 
-    mmm.sec_sale = bank_sec_old - mmm.bank_sec
-    if mmm.sec_sale > 0:
-        mmm.demand_of_stock_b = 0
-        mmm.supply_of_stock_b = mmm.security_sale
-    elif mmm.security_sale < 0:
-        mmm.demand_of_stock_b = mmm.security_sale
-        mmm.supply_of_stock_b = 0
+        mmm = Bank(result.x[4], result.x[0], result.x[3], result.x[1], result.x[2], mmm.deposits, mmm.equity,
+                   mmm.alpha_min, mmm.provision_per, mmm.phi, mmm.zeta, mmm.car, mmm.xs, mmm.xbl, mmm.xl)
     else:
-        mmm.demand_of_stock_b = 0
-        mmm.supply_of_stock_b = 0
+        mmm = Bank(0, 0, 0, 0, 0, 0, 0, mmm.alpha_min, mmm.provision_per, mmm.phi, mmm.zeta, mmm.car, mmm.xs, mmm.xbl,
+                   mmm.xl)
 
-    mmm = Bank(result.x[4], result.x[0], result.x[3], result.x[1], result.x[2], mmm.deposits, mmm.equity,
-               mmm.alpha_min, mmm.provision_per, mmm.phi, mmm.zeta, mmm.car, mmm.xs, mmm.xbl, mmm.xl)
     ########################################################
     # optimixation phase
     ### objective function of shadow banks
@@ -210,7 +213,8 @@ def optimize_shadow_bank(nnn):
         nnn.demand_of_stock = 0
         nnn.supply_of_stock = 0
 
-    nnn = Shadow_Bank(np.random.normal(100), np.random.normal(20), np.random.normal(80), 0.01, 0.01)
+    nnn = Shadow_Bank((result_s.x[0] + result_s.x[1]), result_s.x[0], result_s.x[1], nnn.s_alpha, nnn.s_provision)
+
 
 ###############################################################
 
@@ -244,6 +248,33 @@ for ttt in range(n_sim):
     optimize_bank(bank_pasargad)
     optimize_bank(bank_melal)
 
+    dynamic_bank(bank_melli)
+    dynamic_bank(bank_seppah)
+    dynamic_bank(bank_tosesaderat)
+    dynamic_bank(bank_maskan)
+    dynamic_bank(bank_sanatmadan)
+    dynamic_bank(bank_keshavarzi)
+    dynamic_bank(bank_tosetavon)
+    dynamic_bank(bank_post)
+    dynamic_bank(bank_eghtesadnovin)
+    dynamic_bank(bank_parsian)
+    dynamic_bank(bank_karafarin)
+    dynamic_bank(bank_saman)
+    dynamic_bank(bank_sina)
+    dynamic_bank(bank_khavarmiane)
+    dynamic_bank(bank_shahr)
+    dynamic_bank(bank_dey)
+    dynamic_bank(bank_saderat)
+    dynamic_bank(bank_tejarat)
+    dynamic_bank(bank_mellat)
+    dynamic_bank(bank_refah)
+    dynamic_bank(bank_ayandeh)
+    dynamic_bank(bank_gardeshgary)
+    dynamic_bank(bank_iranzamin)
+    dynamic_bank(bank_sarmaye)
+    dynamic_bank(bank_pasargad)
+    dynamic_bank(bank_melal)
+
     optimize_shadow_bank(shadow1)
     optimize_shadow_bank(shadow2)
     optimize_shadow_bank(shadow3)
@@ -259,8 +290,10 @@ for ttt in range(n_sim):
     optimize_shadow_bank(shadow13)
     optimize_shadow_bank(shadow14)
     optimize_shadow_bank(shadow15)
+
     demand_of_banks = bank_melli.borrow_from_banks + bank_seppah.borrow_from_banks + bank_tosesaderat.borrow_from_banks + bank_maskan.borrow_from_banks + bank_sanatmadan.borrow_from_banks + bank_keshavarzi.borrow_from_banks + bank_tosetavon.borrow_from_banks + bank_post.borrow_from_banks + bank_eghtesadnovin.borrow_from_banks + bank_parsian.borrow_from_banks + bank_karafarin.borrow_from_banks + bank_saman.borrow_from_banks + bank_saman.borrow_from_banks + bank_sina.borrow_from_banks + bank_khavarmiane.borrow_from_banks + bank_shahr.borrow_from_banks + bank_dey.borrow_from_banks + bank_saderat.borrow_from_banks + bank_tejarat.borrow_from_banks + bank_mellat.borrow_from_banks + bank_refah.borrow_from_banks + bank_ayandeh.borrow_from_banks + bank_gardeshgary.borrow_from_banks + bank_iranzamin.borrow_from_banks + bank_sarmaye.borrow_from_banks + bank_sarmaye.borrow_from_banks + bank_pasargad.borrow_from_banks + bank_melal.borrow_from_banks
     supply_of_banks = bank_melli.lend_to_banks + bank_seppah.lend_to_banks + bank_tosesaderat.lend_to_banks + bank_maskan.lend_to_banks + bank_sanatmadan.lend_to_banks + bank_keshavarzi.lend_to_banks + bank_tosetavon.lend_to_banks + bank_post.lend_to_banks + bank_eghtesadnovin.lend_to_banks + bank_parsian.lend_to_banks + bank_karafarin.lend_to_banks + bank_saman.lend_to_banks + bank_saman.lend_to_banks + bank_sina.lend_to_banks + bank_khavarmiane.lend_to_banks + bank_shahr.lend_to_banks + bank_dey.lend_to_banks + bank_saderat.lend_to_banks + bank_tejarat.lend_to_banks + bank_mellat.lend_to_banks + bank_refah.lend_to_banks + bank_ayandeh.lend_to_banks + bank_gardeshgary.lend_to_banks + bank_iranzamin.lend_to_banks + bank_sarmaye.lend_to_banks + bank_sarmaye.lend_to_banks + bank_pasargad.lend_to_banks + bank_melal.lend_to_banks
+    diference = demand_of_banks - supply_of_banks
 
     if demand_of_banks > supply_of_banks:
         rfree = (rfree + rfree_max) / 2
@@ -277,7 +310,9 @@ for ttt in range(n_sim):
     stock_supply_of_shadow_banks = shadow1.supply_of_stock + shadow2.supply_of_stock + shadow3.supply_of_stock + shadow4.supply_of_stock + shadow5.supply_of_stock + shadow6.supply_of_stock + shadow7.supply_of_stock + shadow8.supply_of_stock + shadow9.supply_of_stock + shadow10.supply_of_stock + shadow11.supply_of_stock + shadow12.supply_of_stock + shadow13.supply_of_stock + shadow14.supply_of_stock + shadow15.supply_of_stock
     total_stock_demand = stock_demand_of_banks + stock_demand_of_shadow_banks
     total_stock_supply = stock_supply_of_banks + stock_supply_of_shadow_banks
+
     p_market_old = p_market
+
     if total_stock_demand > total_stock_supply:
         p_market = (p_market + p_market_max) / 2
     elif demand_of_banks < supply_of_banks:
@@ -286,9 +321,11 @@ for ttt in range(n_sim):
         p_market = p_market
 
     ret_on_sec = p_market / p_market_old - 1
+    ret_sec_shbank = ret_on_sec
     p_market_vector.append(p_market)
     ret_sec_bank_vector.append(ret_on_sec)
-
+    every = bank_melli.sec_sale
+    every_thing_vector.append(every)
 
 # print(rfree_vector)
 # print(ret_sec_bank_vector)
@@ -298,52 +335,69 @@ rfree_plot = []
 for i in range(0, len(rfree_vector)):
     rfree_plot.append([float(rfree_vector[i])])
 
-
-
 p_market_plot = []
 for i in range(0, len(p_market_vector)):
     p_market_plot.append([float(p_market_vector[i])])
-
-
 
 ret_on_sec_plot = []
 for i in range(0, len(ret_sec_bank_vector)):
     ret_on_sec_plot.append([float(ret_sec_bank_vector[i])])
 
+every_thing_plot = []
+for i in range(0, len(every_thing_vector)):
+    every_thing_plot.append([float(every_thing_vector[i])])
+
 # print(ret_on_sec_plot)
 # print(p_market_plot)
 # print(rfree_plot)
+# print(every_thing_vector)
 
-# plt.plot(rfree_plot)
-# plt.show()
-plt.plot(p_market_plot)
+
+plt.plot(rfree_plot)
 plt.show()
+# plt.plot(p_market_plot)
+# plt.show()
 # plt.plot(ret_on_sec_plot)
 # plt.show()
-
+plt.plot(every_thing_plot)
+plt.show()
 
 ##### dynamics of model
 # the name of bank which is source of the shock
-# shock_hit = bank_melli
-# sig = 0.01
-# shock = sig * (shock_hit.deposits + shock_hit.borrow_from_banks)
+
+shock_hit = bank_melli
+
+sig = 0.01
+shock = sig * (shock_hit.deposits + shock_hit.borrow_from_banks)
 
 
-# def dynamic_bank(www):
-#     if shock <= www.bank_cash:
-#         landa = sig * (www.deposits + www.borrow_from_banks)
-#         www.equity = www.equity - (www.bank_cash - landa)
-#         www.bank_cash = www.bank_cash - landa
-#     elif (www.bank_cash + www.lend_to_banks) >= shock >= www.bank_cash and www.lend_to_banks != 0:
-#         www.equity = www.equity - www.bank_cash
-#         www.bank_cash = 0
-#         www.lend_to_banks = www.lend_to_banks - (shock - www.bank_cash)
-#     elif (www.bank_cash + www.lend_to_banks + www.bank_sec) >= shock >= (www.bank_cash + www.lend_to_banks):
-#         delta = www.bank_cash + www.lend_to_banks + www.bank_sec - shock
-#         www.bank_cash = 0
-#         www.equity = www.equity - www.bank_cash
-#         www.lend_to_banks = 0
-#         www.bank_sec = www.bank_sec - delta
-#         www.stock = www.bank_sec / p_market
-#     elif (www.bank_cash + www.lend_to_banks + www.bank_sec) <= shock:
-#         www.bankrupt = True
+def dynamic_bank(www):
+    if shock <= www.bank_cash:
+        www.equity = www.equity - shock
+        www.bank_cash = www.bank_cash - shock
+    elif (www.bank_cash + www.lend_to_banks) >= shock >= www.bank_cash:
+        landa = (sig * (www.deposits + www.borrow_from_banks)) - www.bank_cash
+        www.equity = www.equity - www.bank_cash
+        www.bank_cash = 0
+        www.lend_to_banks = www.lend_to_banks - (shock - www.bank_cash)
+    elif (www.bank_cash + www.lend_to_banks + www.bank_sec) >= shock >= (www.bank_cash + www.lend_to_banks):
+        delta = www.bank_cash + www.lend_to_banks + www.bank_sec - shock
+        www.bank_cash = 0
+        www.equity = www.equity - www.bank_cash
+        www.lend_to_banks = 0
+        www.bank_sec = www.bank_sec - delta
+        www.stock = www.bank_sec / p_market
+    elif (www.bank_cash + www.lend_to_banks + www.bank_sec) <= shock:
+        www.bank_cash = 0
+        www.equity = 0
+        www.lend_to_banks = 0
+        www.bank_sec = 0
+        www.borrow_from_banks = 0
+        www.bankrupt = True
+
+
+def dynamic_sh_bank(zzz):
+    if ret_on_sec < 0:
+        mmm
+
+
